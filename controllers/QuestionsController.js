20f9c8e0649cc6a0c13a4506ecd58e73d305ab2a
Jason Creighton creightonja@gmail.com & Kenny Grage kennygrage@gmail.com <student@epicodus.com>
2015-09-23 14:54:50 -0700

    add ability to answer question

--- controllers/QuestionsController.js
+++ controllers/QuestionsController.js
@@ -1,7 +1,8 @@
 questionList.controller('QuestionsCtrl', function QuestionsCtrl($scope, $stateParams, UsersFactory, UtilitiesFactory) {
   $scope.user = UtilitiesFactory.findById(UsersFactory.users, $stateParams.userId);
   $scope.addQuestion = function() {
-    $scope.user.questions.push({ name: $scope.questionName });
+    $scope.user.questions.push({ name: $scope.questionName, answer: $scope.answer });
     $scope.questionName = null;
+    $scope.answer = null;
   }
 });
