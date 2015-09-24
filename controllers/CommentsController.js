b71f12e423093459405c19f2b8e7f912c3dc814b
Jason Creighton creightonja@gmail.com & Kenny Grage kennygrage@gmail.com <student@epicodus.com>
2015-09-24 10:05:24 -0700

    add style

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -1,3 +1,7 @@
 questionList.controller('CommentsCtrl', function CommentsCtrl($scope, $stateParams, UsersFactory, UtilitiesFactory, QuestionsCtrl){
   $scope.user = UtilitiesFactory.findById(UsersFactory.users, $stateParams.userId);
+  $scope.addComment = function() {
+    $scope.user.question.
+  }
 }
+});
--- controllers/QuestionsController.js
+++ controllers/QuestionsController.js
@@ -1,9 +1,10 @@
 questionList.controller('QuestionsCtrl', function QuestionsCtrl($scope, $stateParams, UsersFactory, UtilitiesFactory) {
   $scope.user = UtilitiesFactory.findById(UsersFactory.users, $stateParams.userId);
   $scope.addQuestion = function() {
-    $scope.user.questions.push({ name: $scope.questionName, answer: $scope.answer, vote: 0, comments: [] });
+    $scope.user.questions.push({ name: $scope.questionName, answer: $scope.answer, approval: $scope.approval, vote: 0, comments: [] });
     $scope.questionName = null;
     $scope.answer = null;
+    $scope.approval = false;
   }
 
   $scope.upVote = function(question) {
@@ -12,6 +13,14 @@ questionList.controller('QuestionsCtrl', function QuestionsCtrl($scope, $statePa
 
   $scope.downVote = function(question) {
     question.vote--;
+  }
+
+  $scope.approve = function (question) {
+    question.approval = true;
+  }
+
+  $scope.disapprove = function (question) {
+    question.approval = false;
   }
 
 });
